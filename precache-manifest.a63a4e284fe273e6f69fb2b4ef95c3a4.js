36e70ed17186462374b14c042a7c2aee9f910ca3
Nagarajan <585645@cognizant.com>
2020-04-22 14:00:51 +0530

    Initial commit on tool

---
+++ precache-manifest.a63a4e284fe273e6f69fb2b4ef95c3a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e516895b98fef59004f5d97d316d52f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3d8b5c02b6edf13b01c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbd1624418208abf09a9",
+    "url": "/static/js/2.2833f241.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2833f241.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3d8b5c02b6edf13b01c",
+    "url": "/static/js/main.9d548682.chunk.js"
+  },
+  {
+    "revision": "7108297d10598f33ff60",
+    "url": "/static/js/runtime-main.7c8b0ee6.js"
+  }
+]);
